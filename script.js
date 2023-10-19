2ace6bb7f2e76a14cda6046e6d0322f2bc645138
IsacLindberg1 <isac.lindberg@icloud.com>
2023-10-19 14:58:28 +0200

    19 oktober
    pillade lite koden :+1:

--- script.js
+++ script.js
@@ -4,36 +4,90 @@ function init(){
     let i;
     
     formElem = document.querySelector("form");
-    console.log(formElem);
     formElem.persons.parentNode.style.color = "#999";
     formElem.persons.disabled = true;
 
     for(i = 0; i < formElem.roomType.length; i++){
-        formElem.roomType[i].addEventListener("click", event =>{
+        formElem.roomType[i].addEventListener("click", event => {
             checkIfFamilyRoom();
             checkCost();
         });
     }
+    formElem.nights.addEventListener("click", checkCost);
+
+    for(i = 0; i < formElem.addition.length; i++){
+        formElem.addition[i].addEventListener("click", event => {
+            checkIfFamilyRoom();
+            checkCost();
+        })
+    }
+
+    formElem.campaigncode.addEventListener("focus", promotionCodeIsFocus);
+    formElem.campaigncode.addEventListener("keyup", checkingPromotionCode);
+
+    formElem.zipcode.addEventListener("blur", zipCode);
+
 }
 
 window.onload = init;
 
-
 function checkIfFamilyRoom(){
     if(formElem.roomType[2].checked === true){
         formElem.persons.parentNode.style.color = "#000";
-        formElem.extra.parentNode.style.color = "#999";
+        formElem.addition[2].parentNode.style.color = "#999";
         formElem.persons.disabled = false;
+        formElem.addition[2].disabled = true;
     }else{
         formElem.persons.parentNode.style.color = "#999";
+        formElem.addition[2].parentNode.style.color = "#000";
         formElem.persons.disabled = true;
+        formElem.addition[2].disabled = false;
     }
 }
 
 function checkCost(){
-    let roomType = Number(formElem.roomType.value.split(",")[1])
+    let extra = 0;
+    let roomType = Number(formElem.roomType.value.split(",")[1]);
     let nights = Number(formElem.nights.value);
-    let lakeView = Number(formElem.)
+    
+    for(i = 0; i < formElem.addition.length; i++){
+        
+        if(formElem.addition[i].checked && !formElem.addition[i].disabled){
+            extra += Number(formElem.addition[i].value.split(",")[1]);
+        }
+        
+    }
+
     let totalCost = document.getElementById("totalCost");
-    totalCost.innerHTML = (nights*roomType);
+    totalCost.innerHTML = ((nights * roomType) + (nights * extra));
+}
+
+function checkingPromotionCode(){
+    let regexPromotionCode = /^[A-Za-z]{3}-[0-9]{2}-[A-Za-z]{1}[0-9]{1}$/;
+    console.log(formElem.campaigncode.value);
+    console.log(regexPromotionCode);
+    if(regexPromotionCode.test(formElem.campaigncode.value)){
+        formElem.campaigncode.style.background = "#90EE90";
+    }else{
+        formElem.campaigncode.style.background = "#FFCCCB";
+    }
+}
+
+function promotionCodeIsFocus(){
+    formElem.campaigncode.style.background = "#FFCCCB";
+}
+
+function zipCode(){
+    let regexZipCode = /^\d{3} ?\d{2}$/;
+    let errorMessage = formElem.zipcode.parentNode.parentNode.getElementsByTagName("span")[1];
+    console.log(regexZipCode);
+    if(regexZipCode.test(formElem.zipcode.value) === false){
+        errorMessage.innerHTML = "Fel format. Postnummer anges med fem siffror (0-9).";
+    }else{
+        errorMessage.innerHTML = "";
+    }
+}
+
+function makeCityUpperCase(){
+    
 }
